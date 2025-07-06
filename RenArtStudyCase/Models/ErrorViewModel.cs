2abda5930978837d0d8f5548c0d92ee9abf54264
BerkanGulyagci <bgulyaci@gmail.com>
2025-07-06 13:23:45 +0300

    first commit

---
+++ RenArtStudyCase/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RenArtStudyCase.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
